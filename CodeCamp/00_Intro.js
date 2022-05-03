ba90322828514f81d5d26c045c7749085ee99934
shubhansu-kr <shubhansu2021@gmail.com>
2022-05-03 20:05:10 +0530

    Intro

---
+++ CodeCamp/00_Intro.js
@@ -0,0 +1,25 @@
+const num = 12;
+
+if (num <= 18) {
+    console.log('Too Small');
+}
+else {
+    console.log('Ok');
+}
+
+console.log("Welcome to Node Js fundamentals ");
+
+// __dirname -> path to curernt dir 
+console.log(__dirname);
+
+// __filename -> current file name 
+console.log(__filename);
+
+// require -> Function to use modules 
+console.log(require);
+
+// module -> info about current module (file)
+console.log(module);
+
+// Process -> info about env where program is being executed 
+console.log(process) ;
