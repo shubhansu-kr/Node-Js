24abe122086e00df3ccb4258fdef915205c6dff6
shubhansu-kr <shubhansu2021@gmail.com>
2022-05-04 23:48:55 +0530

    Mind Grenade

---
+++ CodeCamp/04_MindGrenade.js
@@ -0,0 +1,8 @@
+const val = 32 ;
+const val1 = 3245;
+
+const addValue = () => {
+    console.log(val + val1) ;
+}
+
+addValue();
---
+++ CodeCamp/05_Import.js
@@ -0,0 +1,6 @@
+console.log('flag 1');
+require('./04_MindGrenade');
+
+// The imported function is executed right after require statement
+
+console.log("Inside import");
