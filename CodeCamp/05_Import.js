2a4cb504de3fa9a52e3719feb70d3eed44a1b024
shubhansu-kr <shubhansu2021@gmail.com>
2022-05-04 23:52:04 +0530

    Importing js

--- CodeCamp/05_Import.js
+++ CodeCamp/05_Import.js
@@ -4,3 +4,10 @@ require('./04_MindGrenade');
 // The imported function is executed right after require statement
 
 console.log("Inside import");
+
+// When we import module, the module is wrapped in a 
+// function and then exported, so if there are any function calling 
+// or console logs in the imported module then they are executed in 
+// the current module. 
+
+// Will discuss more on this later in the series 
