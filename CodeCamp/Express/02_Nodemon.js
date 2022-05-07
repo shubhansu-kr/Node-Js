28ac04ca1ae55936a3394abeadf6f75e71e5cf4e
shubhansu-kr <shubhansu2021@gmail.com>
2022-05-07 14:26:30 +0530

    Nodemon && uninstall package

---
+++ CodeCamp/Express/02_Nodemon.js
@@ -0,0 +1,17 @@
+// node mon track over the files for us and we dont need to run server
+// again and again 
+
+// We can install nodemon as dev-dependencies 
+// npm i nodemon -D or --save-dev
+
+// scripts in package.json : We can create scripts and run the script
+// using npm <script_name> 
+
+// npm run <script_name> if we are using nodemon script
+// simply restarts the app auto matically , when we change anything in the 
+// file 
+
+// Uninstall a package in npm 
+
+// npm uninstall <package_name> ;
+// or we can edit the package file and run node install
