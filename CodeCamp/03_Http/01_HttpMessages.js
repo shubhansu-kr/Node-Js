a54d06f882087b4764ec72886b900b24a93ffa61
shubhansu-kr <shubhansu2021@gmail.com>
2022-05-30 23:01:56 +0530

    Http Messages

---
+++ CodeCamp/03_Http/01_HttpMessages.js
@@ -0,0 +1,18 @@
+// Client sends a http request message 
+
+// {Method} {URL}
+
+// Message Format 
+// Requested URL : https://www.google.com/search
+// Requested Method : GET 
+// Status Code : 200 Ok
+// Remote Address : 138.32.634.2:443
+// Referrer Plicy : strict-origin-when-cross-origin 
+
+// Methods: 
+
+// GET       www.store.com/api/orders          Get all orders
+// POST      www.store.com/api/orders          Place an order
+// PUT       www.store.com/api/orders/:id      Update specific order
+// GET       www.store.com/api/orders/:id      Get single order 
+// DELETE    www.store.com/api/orders/:id      Delete order 
